3f2ee36094d1c1fa88767a92c92ae8df2a30d5db
Oliver Jourmel <oliver@harpcanada.com>
2012-11-26 22:16:07 -0800

    Adds the testPlan for OU, and removes old code from Output.c/fixes tabs

--- Output/Output.c
+++ Output/Output.c
@@ -24,51 +24,26 @@ void OUPrint(void)
    char tempnewstring[80];
 
 	for (i = 0; i < SSNumLines(); i++) 
-   {
+	{
 		int numWords = SSNumWords(i);
-      strcpy(temporigstring, "");
-      strcpy(tempnewstring, "");
+		strcpy(temporigstring, "");
+		strcpy(tempnewstring, "");
   
-      for(h = 0; h < (numWords-SSGetShiftNum(i)); h++)
-      {
-           strcat(temporigstring, SSGetWord(i,h));
-           strcat(temporigstring, " ");
-      }
-      
-      for(h = (numWords-SSGetShiftNum(i)); h < (numWords); h++)
-      {
-           strcat(tempnewstring, SSGetWord(i,h));
-           if (h != SSNumWords(i)-1)
-           {
-               strcat(tempnewstring, " ");
-           }
-      }
-
-   //   printf(">>>>>%s\n", temporigstring);
-   //   printf(">>>>>%s\n", tempnewstring);
-      
-      printf("%40s %-40s\n", tempnewstring ,  temporigstring);
-   /*
-		for (j = 0; j < numWords; j++) {
-			int shiftNum = SSGetShiftNum(i);
-         
-         printf("%d", SSGetShiftNum(i));
-         
-        // strcpy(tempstring, SSGetWord(i,j));
-			printf("%s",SSGetWord(i,j));
-
-			// no comma if line is unshifted
-			//  otherwise comma follows last word on original line
-			 
-			if (shiftNum != 0 && j == numWords-shiftNum-1)
-				printf(",");
-
-			// space follows every word but the last 
-			if (j != SSNumWords(i)-1)
-				printf(" ");
+		for(h = 0; h < (numWords-SSGetShiftNum(i)); h++)
+		{
+			strcat(temporigstring, SSGetWord(i,h));
+			strcat(temporigstring, " ");
 		}
-		printf("\n");
-      
-      */
+	 
+		for(h = (numWords-SSGetShiftNum(i)); h < (numWords); h++)
+		{
+			strcat(tempnewstring, SSGetWord(i,h));
+			if (h != SSNumWords(i)-1)
+			{
+				strcat(tempnewstring, " ");
+			}
+		}
+	 
+		printf("%40s %-40s\n", tempnewstring ,  temporigstring);
 	}
 }
